c5a3ed90605d7db14fb6c85a0062d0a4d858bc28
host4ideas <72515482+host4ideas@users.noreply.github.com>
2022-11-21 14:03:26 +0100

    examen vue terminado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap-vue/dist/bootstrap-vue";
+
+const app = createApp(App);
+app.use(router);
+
+router.isReady().then(() => {
+    app.mount("#app");
+});
